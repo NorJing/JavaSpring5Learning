26834ec2404044aa04a32a3570f8b5760046d2dd
doyan <doyan@nets.eu>
2020-02-18 13:43:18 +0100

    little kafka

---
+++ Kafka-HelloWorld-1/src/main/java/com/learning/KafkaHelloWorld1Application.java
@@ -0,0 +1,13 @@
+package com.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaHelloWorld1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaHelloWorld1Application.class, args);
+	}
+
+}
